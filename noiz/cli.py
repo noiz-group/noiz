8864d5409a35fa7578e3c56621a531225f911b97
Damian Kula <dkula@unistra.fr>
2019-04-02 17:53:05 +0200

    adds option add default config

--- noiz/cli.py
+++ noiz/cli.py
@@ -1,21 +1,24 @@
 import click
-from flask import Flask
+from flask import Flask, current_app
 from flask.cli import AppGroup, FlaskGroup
 
-user_cli = AppGroup('noiz')
+from .models import ProcessingConfig
+from .extensions import db
+
+init = AppGroup('init', help='Performs actions to initiate')
 flask_custom_cli = AppGroup('noizfff')
 
-@user_cli.group("This is explanation of the first group")
+@init.group("""Introductory actions in the noiz app""")
 def user_cli_group():
-    '''This is short explanation?'''
     pass
 
-@user_cli.command()
-def first():
+@init.command('populate_config')
+def populate_config():
     '''That's the explanation of first command of the group'''
-    click.echo("That's the first command of the group")
 
-@user_cli.command()
+    upsert_default_config()
+
+@init.command()
 def second():
     '''That's the explanation of second command of the group'''
     click.echo("That's the second command of the group")
@@ -34,16 +37,52 @@ def firstf():
     click.echo("That's the first command of the group")
 
 
-# @click.command()
-# @click.pass_context
-# def init_db():
-#     app = create_app()
-#     db.create_all(app)
-
 @flask_custom_cli.command()
 # @click.pass_context
 def secondf():
     '''That's the explanation of second command of the group'''
     click.echo("That's the second command of the group")
 
-# app.cli.add_command(user_cli)
+
+
+def upsert_default_config():
+
+    default_config = ProcessingConfig(
+            use_winter_time=False,
+            f_sampling_out=25,
+            downsample_filt_order=3,
+            window_length_spectrum_sec=100,
+            window_spectrum_overlap=0.1,
+            window_spectrum_reject_method=None,
+            window_spectrum_crit_std=3,
+            window_spectrum_crit_quantile=0.1,
+            window_spectrum_num_for_stat=10,
+            sequence_length_sec=1800,
+            sequence_overlap=0,
+            sequence_reject_method=None,
+            sequence_crit_std=0,
+            sequence_crit_quantile=0,
+            sequence_num_for_stat=10,
+            sequence_reject_tolerance_on_window_spectrum=5,
+            f_min_reject_crit=0.01,
+            f_max_reject_crit=12,
+            filter_type='butterworth',
+            taper_time_type='cosine',
+            taper_time_width_periods=200,
+            taper_time_width_min_samples=25,
+            taper_time_width_max_proportion=0.1,
+            taper_freq_type='cosine',
+            taper_freq_width_proportion=0.1,
+            taper_freq_width_min_samples=100,
+            taper_freq_width_max_freqs=25,
+        )
+
+    current_config = db.session.query(ProcessingConfig).first()
+
+    if  current_config is not None:
+        db.session.delete(current_config)
+
+    db.session.add(default_config)
+    db.session.commit()
+
+    return
