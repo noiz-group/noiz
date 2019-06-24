6d3cdfaaf365d4024fc98cc091b8c4dca412c197
Damian Kula <dkula@unistra.fr>
2019-06-24 21:53:51 +0200

    mostly line endings

--- noiz/database.py
+++ noiz/database.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """Database module, including the SQLAlchemy database object and DB-related utilities."""
-from noiz.extensions import db
 
 # Alias common SQLAlchemy names
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
 Column = db.Column
 relationship = db.relationship
