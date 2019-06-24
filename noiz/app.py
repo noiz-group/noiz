1dff46701e7518b425109b4aab323a5501fa1b06
Damian Kula <dkula@unistra.fr>
2019-06-24 22:02:50 +0200

    adds logging, and some clearing in app.py

--- noiz/app.py
+++ noiz/app.py
@@ -1,38 +1,57 @@
 from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
 
 from noiz.routes import simple_page
-from noiz.cli import cli
-from noiz.extensions import db, migrate
+from noiz.database import db, migrate
 
-from celery import Celery
+from noiz.models import ProcessingConfig, File
+
+
 
 def create_app(config_object="noiz.settings"):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    from noiz.models.processingconfig import ProcessingConfig
-    from noiz.models.file import File
+    from noiz.models import ProcessingConfig, File
 
     register_extensions(app)
     register_blueprints(app)
     register_cli_extensions(app)
+    configure_logger(app)
     return app
+
 
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
     return None
 
+
 def register_blueprints(app):
     app.register_blueprint(simple_page)
     return None
+
 
 def register_cli_extensions(app):
     # app.cli.add_command(cli)
 
     return None
 
+def configure_logger(app):
+    """Configure loggers."""
+    formatter = logging.Formatter(app.config['LOG_FORMATTER'])
 
+    stream_handler = logging.StreamHandler(sys.stdout)
+    rot_file_handler = RotatingFileHandler(app.config['LOG_FILE'], maxBytes=10000, backupCount=1)
+
+
+    if not app.logger.handlers:
+        for handler in (stream_handler, rot_file_handler):
+            handler.setFormatter(formatter)
+            handler.setLevel(app.config['LOG_LEVEL'])
+            app.logger.addHandler(handler)
 
 # # create and configure the app
 # app = Flask(__name__, instance_relative_config=True)
