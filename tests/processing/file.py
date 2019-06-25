e4607bf63ac7a3da9ef37e7abf393f27644e0d35
Damian Kula <dkula@unistra.fr>
2019-06-25 14:47:45 +0200

    Adds dummy tests

---
+++ tests/processing/file.py
@@ -0,0 +1,22 @@
+import pytest
+
+from noiz.processing.file import (search_for_seismic_files,
+    search_recursively_insert_seismic_files,
+    insert_single_seismic_file,
+    _get_existing_filepaths)
+
+
+def test_search_for_seismic_files():
+    assert False
+
+
+def test_search_recursively_insert_seismic_files():
+    assert False
+
+
+def test_insert_single_seismic_file():
+    assert False
+
+
+def test__get_existing_filepaths():
+    assert False
