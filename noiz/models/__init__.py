cdc6f2623480a33b9dbf4e5210e91b37a7637700
Damian Kula <dkula@unistra.fr>
2019-10-31 10:17:56 +0100

    Forgotten place of renaming a processing_config class

--- noiz/models/__init__.py
+++ noiz/models/__init__.py
@@ -1,6 +1,6 @@
 from noiz.models.timespan import Timespan
 from noiz.models.component import Component
-from noiz.models.processing_config import ProcessingConfig
+from noiz.models.processing_params import ProcessingParams
 from noiz.models.time_series_index import Tsindex
 from noiz.models.soh import Soh
 from noiz.models.datachunk import DataChunk
