ca9e8f01a1bfe4bd0e0032229d575761bc1e9b19
Damian Kula <dkula@unistra.fr>
2019-10-31 10:18:46 +0100

    Removes nullability, adds field for processing_config ID

--- noiz/models/datachunk.py
+++ noiz/models/datachunk.py
@@ -13,11 +13,22 @@ class DataChunk(db.Model):
         db.UniqueConstraint(
             "timespan_id",
             "component_id",
-            name="unique_datachunk_per_timespan_per_station",
+            "processing_params_id",
+            name="unique_datachunk_per_timespan_per_station_per_processing",
         ),
     )
-    component_id = db.Column("component_id", db.Integer, db.ForeignKey("component.id"))
-    timespan_id = db.Column("timespan_id", db.BigInteger, db.ForeignKey("timespan.id"))
-    sampling_rate = db.Column("sampling_rate", db.Float)
-    npts = db.Column("npts", db.Integer)
-    filepath = db.Column("filepath", db.UnicodeText)
+    component_id = db.Column(
+        "component_id", db.Integer, db.ForeignKey("component.id"), nullable=False
+    )
+    processing_params_id = db.Column(
+        "processing_params_id",
+        db.Integer,
+        db.ForeignKey("processing_params.id"),
+        nullable=False,
+    )
+    timespan_id = db.Column(
+        "timespan_id", db.BigInteger, db.ForeignKey("timespan.id"), nullable=False
+    )
+    sampling_rate = db.Column("sampling_rate", db.Float, nullable=False)
+    npts = db.Column("npts", db.Integer, nullable=False)
+    filepath = db.Column("filepath", db.UnicodeText, nullable=False)
