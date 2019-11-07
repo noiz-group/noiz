00d27d92b13813238e5b30317935e1d360ca35fb
Damian Kula <dkula@unistra.fr>
2019-11-07 17:06:48 +0100

    Adds component_pair model

--- noiz/models/__init__.py
+++ noiz/models/__init__.py
@@ -4,3 +4,4 @@ from noiz.models.processing_params import ProcessingParams
 from noiz.models.time_series_index import Tsindex
 from noiz.models.soh import Soh
 from noiz.models.datachunk import DataChunk, ProcessedDatachunk
+from noiz.models.component_pair import ComponentPair
---
+++ noiz/models/component_pair.py
@@ -0,0 +1,52 @@
+from noiz.database import db
+
+
+class ComponentPair(db.Model):
+    __tablename__ = "componentpair"
+
+    id = db.Column("id", db.Integer, primary_key=True)
+    component_a_id = db.Column(
+        "component_a_id", db.Integer, db.ForeignKey("component.id"), nullable=False
+    )
+    component_b_id = db.Column(
+        "component_b_id", db.Integer, db.ForeignKey("component.id"), nullable=False
+    )
+    autocorrelation = db.Column("autocorrelation", db.Boolean, nullable=False)
+    intracorrelation = db.Column("intracorrelation", db.Boolean, nullable=False)
+    azimuth = db.Column("azimuth", db.Float, nullable=False)
+    backazimuth = db.Column("backazimuth", db.Float, nullable=False)
+    distance = db.Column("distance", db.Float, nullable=False)
+
+    def _set_same_station(self) -> None:
+        self.azimuth = 0
+        self.backazimuth = 0
+        self.distance = 0
+        return
+
+    def set_autocorrelation(self) -> None:
+        self.autocorrelation = True
+        self.intracorrelation = False
+        self._set_same_station()
+        return
+
+    def set_intracorrelation(self) -> None:
+        self.autocorrelation = False
+        self.intracorrelation = True
+        self._set_same_station()
+        return
+
+    def set_params_from_distaz(self, distaz: dict) -> None:
+        self.azimuth = distaz["azimuth"]
+        self.backazimuth = distaz["backazimuth"]
+        self.distance = distaz["distance"]
+        self.autocorrelation = False
+        self.intracorrelation = False
+        if not self._verify_east_west():
+            raise ValueError("This pair is not east west in the end.")
+        return
+
+    def _verify_east_west(self) -> bool:
+        if self.backazimuth >= 180:
+            return True
+        else:
+            return False
