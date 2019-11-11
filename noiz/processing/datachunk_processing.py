f15f49223b179fdbf8f7add1a4d65ae759a8c396
Damian Kula <dkula@unistra.fr>
2019-11-11 11:45:50 +0100

    Extract processing datachunk into method

--- noiz/processing/datachunk_processing.py
+++ noiz/processing/datachunk_processing.py
@@ -74,71 +74,71 @@ def run_chunk_processing(
 
         for i, dc in enumerate(datachunks):
             logging.info(f"Processing datachunk {i}/{no_datachunks}")
-            logging.info(f"Loading data for {dc}")
-            st = dc.load_data()
-            if len(st) != 1:
-                logging.info(
-                    f"There are more than one trace in stream, trying to merge with default params"
-                )
-                st.merge()
-                if len(st) != 1:
-                    raise ValueError("There still are more traces in stream than one.")
-            if processing_params.spectral_whitening:
-                logging.info("Performing spectral whitening")
-                st[0] = whiten_trace(st[0])
-            if processing_params.one_bit:
-                logging.info("Performing one bit normalization")
-                st[0] = one_bit_normalization(st[0])
+            process_datachunk(dc, processing_params)
 
-            new_filepath = dc.filepath.replace("datachunk", "processed_datachunk")
-            logging.info(f"Output file will be saved to {new_filepath}")
-            directory_exists_or_create(Path(new_filepath))
+    return
 
-            proc_dc = ProcessedDatachunk(
-                processing_params_id=processing_params.id,
-                datachunk_id=dc.id,
-                filepath=new_filepath,
-            )
 
+def process_datachunk(datachunk, processing_params):
+    logging.info(f"Loading data for {datachunk}")
+    st = datachunk.load_data()
+    if len(st) != 1:
+        logging.info(
+            f"There are more than one trace in stream, trying to merge with default params"
+        )
+        st.merge()
+        if len(st) != 1:
+            raise ValueError("There still are more traces in stream than one.")
+    if processing_params.spectral_whitening:
+        logging.info("Performing spectral whitening")
+        st[0] = whiten_trace(st[0])
+    if processing_params.one_bit:
+        logging.info("Performing one bit normalization")
+        st[0] = one_bit_normalization(st[0])
+    new_filepath = datachunk.filepath.replace("datachunk", "processed_datachunk")
+    logging.info(f"Output file will be saved to {new_filepath}")
+    directory_exists_or_create(Path(new_filepath))
+    proc_dc = ProcessedDatachunk(
+        processing_params_id=processing_params.id,
+        datachunk_id=datachunk.id,
+        filepath=new_filepath,
+    )
+    logging.info(
+        "Checking if there are some chunks fot tht timespan and component in db"
+    )
+    existing_chunks = (
+        db.session.query(ProcessedDatachunk)
+        .filter(
+            ProcessedDatachunk.datachunk_id == proc_dc.datachunk_id,
+            ProcessedDatachunk.processing_params_id == proc_dc.processing_params_id,
+        )
+        .all()
+    )
+    logging.info(
+        "Checking if there are some timeseries files  for tht timespan and component on the disc"
+    )
+    if len(existing_chunks) == 0:
+        logging.info("Writing file to disc and adding entry to db")
+        st.write(proc_dc.filepath, format="mseed")
+        db.session.add(proc_dc)
+    else:
+        if not Path(proc_dc.filepath).exists():
             logging.info(
-                "Checking if there are some chunks fot tht timespan and component in db"
+                "There is some chunk in the db so I will update it and write/overwrite file to the disc."
             )
-
-            existing_chunks = (
-                db.session.query(ProcessedDatachunk)
-                .filter(
-                    ProcessedDatachunk.datachunk_id == proc_dc.datachunk_id,
-                    ProcessedDatachunk.processing_params_id
-                    == proc_dc.processing_params_id,
-                )
-                .all()
+        st.write(proc_dc.filepath, format="mseed")
+        insert_command = (
+            insert(ProcessedDatachunk)
+            .values(
+                processing_params_id=proc_dc.processing_params_id,
+                datachunk_id=proc_dc.datachunk_id,
+                filepath=proc_dc.filepath,
             )
-            logging.info(
-                "Checking if there are some timeseries files  for tht timespan and component on the disc"
+            .on_conflict_do_update(
+                constraint="unique_processing_per_datachunk",
+                set_=dict(filepath=proc_dc.filepath),
             )
-            if len(existing_chunks) == 0:
-                logging.info("Writing file to disc and adding entry to db")
-                st.write(proc_dc.filepath, format="mseed")
-                db.session.add(proc_dc)
-            else:
-                if not Path(proc_dc.filepath).exists():
-                    logging.info(
-                        "There is some chunk in the db so I will update it and write/overwrite file to the disc."
-                    )
-                    st.write(proc_dc.filepath, format="mseed")
-                    insert_command = (
-                        insert(ProcessedDatachunk)
-                        .values(
-                            processing_params_id=proc_dc.processing_params_id,
-                            datachunk_id=proc_dc.datachunk_id,
-                            filepath=proc_dc.filepath,
-                        )
-                        .on_conflict_do_update(
-                            constraint="unique_processing_per_datachunk",
-                            set_=dict(filepath=proc_dc.filepath),
-                        )
-                    )
-                    db.session.execute(insert_command)
-            db.session.commit()
-
+        )
+        db.session.execute(insert_command)
+    db.session.commit()
     return
