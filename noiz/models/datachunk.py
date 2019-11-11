920d5fc0c40eca567d2935f5479f76c64c32d438
Damian Kula <dkula@unistra.fr>
2019-11-11 11:40:36 +0100

    Extract No data exception to common exception file, adds padded npts column to datachunk

--- noiz/models/datachunk.py
+++ noiz/models/datachunk.py
@@ -1,3 +1,4 @@
+from noiz.exceptions import MissingDataFileException
 from noiz.database import db
 
 from flask.logging import logging
@@ -8,14 +9,8 @@ import obspy
 logger = logging.getLogger(__name__)
 
 
-class MissingDataFile(Exception):
-    pass
-
-
 class DataChunk(db.Model):
     __tablename__ = "datachunk"
-
-    id = db.Column("id", db.BigInteger, primary_key=True)
     __table_args__ = (
         db.UniqueConstraint(
             "timespan_id",
@@ -24,6 +19,8 @@ class DataChunk(db.Model):
             name="unique_datachunk_per_timespan_per_station_per_processing",
         ),
     )
+
+    id = db.Column("id", db.BigInteger, primary_key=True)
     component_id = db.Column(
         "component_id", db.Integer, db.ForeignKey("component.id"), nullable=False
     )
@@ -39,6 +36,7 @@ class DataChunk(db.Model):
     sampling_rate = db.Column("sampling_rate", db.Float, nullable=False)
     npts = db.Column("npts", db.Integer, nullable=False)
     filepath = db.Column("filepath", db.UnicodeText, nullable=False)
+    padded_npts = db.Column("padded_npts", db.Integer, nullable=True)
 
     timespan = db.relationship("Timespan", foreign_keys=[timespan_id])
     component = db.relationship("Component", foreign_keys=[component_id])
@@ -51,7 +49,7 @@ class DataChunk(db.Model):
         if Path(self.filepath).exists:
             return obspy.read(self.filepath, "MSEED")
         else:
-            raise MissingDataFile(f"Data file for chunk {self} is missing")
+            raise MissingDataFileException(f"Data file for chunk {self} is missing")
 
 
 class ProcessedDatachunk(db.Model):
@@ -82,4 +80,4 @@ class ProcessedDatachunk(db.Model):
         if Path(self.filepath).exists:
             return obspy.read(self.filepath, "MSEED")
         else:
-            raise MissingDataFile(f"Data file for chunk {self} is missing")
+            raise MissingDataFileException(f"Data file for chunk {self} is missing")
