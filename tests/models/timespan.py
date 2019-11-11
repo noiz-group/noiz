de171e299353c296aac1c5a73755287b8d1ba0dd
Damian Kula <dkula@unistra.fr>
2019-11-11 16:34:15 +0100

    Adds test stubs for timespan methods

---
+++ tests/models/timespan.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+class TestTimespan:
+    def test_starttime_year(self):
+        assert False
+
+    def test_starttime_doy(self):
+        assert False
+
+    def test_midtime_year(self):
+        assert False
+
+    def test_midtime_doy(self):
+        assert False
+
+    def test_endtime_year(self):
+        assert False
+
+    def test_endtime_doy(self):
+        assert False
+
+    def test_remove_last_nanosecond(self):
+        assert False
+
+    def test_same_day(self):
+        assert False
+
+    def test_starttime_obspy(self):
+        assert False
+
+    def test_midtime_obspy(self):
+        assert False
+
+    def test_endtime_obspy(self):
+        assert False
