d2f34c019ea954680619321b7e947b3f6bb8cf84
Damian Kula <dkula@unistra.fr>
2019-11-12 12:35:52 +0100

    Adds test stubs for componentpair model

---
+++ tests/models/test_component_pair.py
@@ -0,0 +1,20 @@
+import pytest
+
+from noiz.models import ComponentPair
+
+
+class TestComponentPair:
+    def test__set_same_station(self):
+        assert False
+
+    def test_set_autocorrelation(self):
+        assert False
+
+    def test_set_intracorrelation(self):
+        assert False
+
+    def test_set_params_from_distaz(self):
+        assert False
+
+    def test__verify_east_west(self):
+        assert False
