355f685b6c8fb3d8149adeb1d0c791a4e21b2b0b
Damian Kula <dkula@unistra.fr>
2019-11-26 15:52:41 +0100

    adds docstrings and typing

--- noiz/processing/inventory.py
+++ noiz/processing/inventory.py
@@ -1,12 +1,33 @@
 import obspy
 from pathlib import Path
+from typing import Iterable, Dict
+from obspy import Inventory
+from obspy.core.inventory.network import Network
+from obspy.core.inventory.station import Station
+from obspy.core.inventory.channel import Channel
 
 import logging
 
 
 def _assembly_single_component_inventory(
-    inventory_to_clean, network_to_clean, station_to_clean, components
-):
+    inventory_to_clean: Inventory,
+    network_to_clean: Network,
+    station_to_clean: Station,
+    components: Iterable[Channel],
+) -> Inventory:
+    """
+    Makes a new inventory object on basis of provided sub objects
+    :param inventory_to_clean:
+    :type inventory_to_clean: Inventory
+    :param network_to_clean:
+    :type network_to_clean: Network
+    :param station_to_clean:
+    :type station_to_clean: Station
+    :param components:
+    :type components: Iterable[Channel]
+    :return:
+    :rtype: Inventory
+    """
     network_new = network_to_clean.copy()
     station_new = station_to_clean.copy()
     station_new.channels = components
@@ -17,7 +38,18 @@ def _assembly_single_component_inventory(
     return inventory_single_component
 
 
-def divide_channels_by_component(channels):
+def divide_channels_by_component(
+    channels: Iterable[Channel]
+) -> Dict[str : Iterable[Channel]]:
+    """
+    Groups provided channels by component letter (last letter of channel code)
+
+    :param channels: Iterable with channels to be groupped
+    :type channels: Iterable[Channel]
+    :return: Dict where key is component letter and items are iterables with Channels
+    :rtype: Dict[str: Iterable[Channel]]
+    """
+
     logging.info("Parsing channels to find components")
     components = {}
     for channel in channels:
@@ -29,7 +61,10 @@ def divide_channels_by_component(channels):
     return components
 
 
-def _assembly_stationxml_filename(network, station, component):
+def _assembly_stationxml_filename(
+    network: Network, station: Station, component: str
+) -> str:
+    """"""
     return f"inventory_{network.code}.{station.code}.{component}.xml"
 
 
