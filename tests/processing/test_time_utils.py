9f0c56054bbe4e5cf20a21ba6cd161b1c54fa1f8
Damian Kula <dkula@unistra.fr>
2019-11-27 12:25:56 +0100

    Fixes an input date to be proper
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- tests/processing/test_time_utils.py
+++ tests/processing/test_time_utils.py
@@ -22,7 +22,7 @@ def test_get_year_doy(test_date, year, doy):
 
 @pytest.mark.parametrize(
     "test_date",
-    (datetime(2019, 1, 1), pd.Timestamp(2017, 10, 30), np.datetime64("2019-15-07")),
+    (datetime(2019, 1, 1), pd.Timestamp(2017, 10, 30), np.datetime64("2019-05-07")),
 )
 def test_validate_timestamp(test_date):
     assert isinstance(validate_timestamp(test_date), pd.Timestamp)
