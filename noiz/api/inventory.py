7f5c352f203f16f0f7453eb892c95e8d0ac544b8
Damian Kula <dkula@unistra.fr>
2019-11-28 17:22:00 +0100

    Removes unnecessary context manager

--- noiz/api/inventory.py
+++ noiz/api/inventory.py
@@ -70,8 +70,7 @@ def parse_inventory_insert_stations_and_components_into_db(app, inventory):
     objects_to_commit, added_filepaths = parse_inventory_for_single_component_db_entries(
         inventory, inventory_dir
     )
-    with app.app_context() as ctx:
-        for obj in objects_to_commit:
-            db.session.merge(obj)
-        db.session.commit()
+    for obj in objects_to_commit:
+        db.session.merge(obj)
+    db.session.commit()
     return
