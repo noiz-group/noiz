0d1b750c4789ad477eb88667dc1fef38a224a3cf
Damian Kula <dkula@unistra.fr>
2019-11-29 15:16:56 +0100

    Renames class DataChunk to Datachunk

--- noiz/models/__init__.py
+++ noiz/models/__init__.py
@@ -3,7 +3,7 @@ from noiz.models.component import Component
 from noiz.models.processing_params import ProcessingParams
 from noiz.models.time_series_index import Tsindex
 from noiz.models.soh import Soh
-from noiz.models.datachunk import DataChunk, ProcessedDatachunk
+from noiz.models.datachunk import Datachunk, ProcessedDatachunk
 from noiz.models.component_pair import ComponentPair
 from noiz.models.crosscorrelation import Crosscorrelation
 from noiz.models.stacking import CCFStack, StackingSchema, StackingTimespan
