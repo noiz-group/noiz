2aac332f59cc9f84fccf05d97ced7da942aa866e
Damian Kula <dkula@unistra.fr>
2019-12-06 11:26:10 +0100

    Adds default None values to fetchers and raising exception if None are provided, imports it in main package

--- noiz/api/__init__.py
+++ noiz/api/__init__.py
@@ -1,3 +1,4 @@
 from noiz.api.component_pair import fetch_component_pairs
 from noiz.api.timespan import fetch_timespans_for_doy
 from noiz.api.datachunk import fetch_datachunks_for_timespan
+from noiz.api.component import fetch_components, fetch_components_by_id
