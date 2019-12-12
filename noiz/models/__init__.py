f713f4035b6805ba04735a9fca8e88a16183b736
Damian Kula <dkula@unistra.fr>
2019-12-12 17:45:59 +0100

    Adds compatibility import of Datachunk as DataChunk

--- noiz/models/__init__.py
+++ noiz/models/__init__.py
@@ -4,6 +4,7 @@ from noiz.models.processing_params import ProcessingParams
 from noiz.models.time_series_index import Tsindex
 from noiz.models.soh import Soh
 from noiz.models.datachunk import Datachunk, ProcessedDatachunk
+from noiz.models.datachunk import Datachunk as DataChunk
 from noiz.models.component_pair import ComponentPair
 from noiz.models.crosscorrelation import Crosscorrelation
 from noiz.models.stacking import CCFStack, StackingSchema, StackingTimespan
