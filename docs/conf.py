6d7cfd3a4c92ff96a69a70147cc6b70203f5c07a
Damian Kula <dkula@unistra.fr>
2019-12-13 14:50:22 +0100

    Relative version and package name and stuff

--- docs/conf.py
+++ docs/conf.py
@@ -10,17 +10,24 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+from datetime import date
+
+file_loc = os.path.split(__file__)[0]
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(file_loc), ".")))
+
+import noiz
 
 
 # -- Project information -----------------------------------------------------
 
-project = "noiz"
-copyright = "2019, Damian Kula"
-author = "Damian Kula"
+project = f"{noiz.__package__}"
+copyright = f"Copyright 2019 -- {date.today().year}, {noiz.__author__}"
+author = f"{noiz.__author__}"
 
+version = f"{noiz.__version__}"
+release = version
 
 # -- General configuration ---------------------------------------------------
 
@@ -51,7 +58,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "groundwork"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
