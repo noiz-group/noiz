dcf6d29b991d32a3b2c5c467eba4a4fd3e71d1fe
Damian Kula <dkula@unistra.fr>
2020-09-29 17:22:30 +0200

    Adds scaffolding of test for each of the methods, xfailed
    Signed-off-by: Damian Kula <dkula@unistra.fr>

---
+++ tests/processing/test_datachunk_preparation.py
@@ -0,0 +1,99 @@
+import pytest
+
+@pytest.mark.xfail
+def test_assembly_sds_like_dir():
+    assert False
+
+
+@pytest.mark.xfail
+def test_expected_npts():
+    assert False
+
+
+@pytest.mark.xfail
+def test_assembly_preprocessing_filename():
+    assert False
+
+
+@pytest.mark.xfail
+def test_assembly_filepath():
+    assert False
+
+
+@pytest.mark.xfail
+def test_directory_exists_or_create():
+    assert False
+
+
+def test_increment_filename_counter():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_next_pow_2():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_resample_with_padding():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_merge_traces_fill_zeros():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_pad_zeros_to_exact_time_bounds():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_preprocess_timespan():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_create_datachunks_add_to_db():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_create_datachunks_for_component():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_add_or_upsert_datachunks_in_db():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice_mutli_traces_mergeable():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice_multi_traces_not_mergeabe():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice_not_enough_samples():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice_samples_over_minimum():
+    assert False
+
+
+@pytest.mark.xfail 
+def test_validate_slice_no_traces_inside():
+    assert False
