2fd819283de47dd7ab45c6d76df80d8d07ac8e65
Damian Kula <dkula@unistra.fr>
2020-10-15 18:12:24 +0200

    Removes the noiz.api.datachunk package due to import problems
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- noiz/api/__init__.py
+++ noiz/api/__init__.py
@@ -1,4 +1,6 @@
 from noiz.api.component_pair import fetch_component_pairs
 from noiz.api.timespan import fetch_timespans_for_doy
-from noiz.api.datachunk.datachunk import fetch_datachunks_for_timespan
 from noiz.api.component import fetch_components, fetch_components_by_id
+from noiz.api.datachunk import fetch_datachunks
+
+from noiz.api.datachunk_plotting import plot_datachunk_availability
--- noiz/api/datachunk/__init__.py
+++
@@ -1,6 +0,0 @@
-from noiz.api.datachunk.datachunk import fetch_datachunks, count_datachunks, \
-    fetch_datachunks_for_timespan, add_or_upsert_datachunks_in_db, \
-    create_datachunks_add_to_db, create_datachunks_for_component, \
-    prepare_datachunk_preparation_parameter_lists, \
-    run_paralel_chunk_preparation, run_chunk_preparation
-from noiz.api.datachunk.plotting import plot_datachunk_availability
