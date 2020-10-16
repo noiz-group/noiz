00077d145f328cab94211dc661e2955732ae2fa2
Damian Kula <dkula@unistra.fr>
2020-10-16 14:27:56 +0200

    Adds SohGPS class
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/models/soh.py
+++ src/noiz/models/soh.py
@@ -2,7 +2,7 @@ from noiz.database import db
 
 
 class SohEnvironment(db.Model):
-    __tablename__ = "soh"
+    __tablename__ = "soh_environment"
     __table_args__ = (
         db.UniqueConstraint(
             "datetime", "component_id", name="unique_timestamp_per_station"
@@ -22,4 +22,26 @@ class SohEnvironment(db.Model):
             "voltage": self.voltage,
             "current": self.current,
             "temperature": self.temperature,
+        }
+
+
+class SohGps(db.Model):
+    __tablename__ = "soh_gps"
+    __table_args__ = (
+        db.UniqueConstraint(
+            "datetime", "component_id", name="unique_timestamp_per_station"
+        ),
+    )
+
+    id = db.Column("id", db.BigInteger, primary_key=True)
+    component_id = db.Column("component_id", db.Integer, db.ForeignKey("component.id"))
+    datetime = db.Column("datetime", db.TIMESTAMP(timezone=True), nullable=False)
+    time_error = db.Column("voltage", db.Integer, nullable=True)
+    time_uncertainty = db.Column("voltage", db.Integer, nullable=True)
+
+    def to_dict(self):
+        return {
+            "datetime": self.datetime,
+            "time_error": self.time_error,
+            "time_uncertainty": self.time_uncertainty,
         }
