7df426c84162bbf03791cf9137b0ba43007de149
Damian Kula <dkula@unistra.fr>
2020-10-16 14:55:18 +0200

    Adds association table and possibility of connecting with one entry more components than one, leaves the explicit z component anyway
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/models/soh.py
+++ src/noiz/models/soh.py
@@ -1,20 +1,32 @@
 from noiz.database import db
 
 
+association_table_soh_env = db.Table(
+    "soh_environment_association",
+    db.metadata,
+    db.Column(
+        "component_id", db.BigInteger, db.ForeignKey("component.id")
+    ),
+    db.Column("soh_environment_id", db.BigInteger, db.ForeignKey("soh_environment.id")),
+)
+
+
 class SohEnvironment(db.Model):
     __tablename__ = "soh_environment"
     __table_args__ = (
         db.UniqueConstraint(
-            "datetime", "component_id", name="unique_timestamp_per_station"
+            "datetime", "z_component_id", name="unique_timestamp_per_station"
         ),
     )
 
     id = db.Column("id", db.BigInteger, primary_key=True)
-    component_id = db.Column("component_id", db.Integer, db.ForeignKey("component.id"))
+    z_component_id = db.Column("z_component_id", db.Integer, db.ForeignKey("component.id"))
     datetime = db.Column("datetime", db.TIMESTAMP(timezone=True), nullable=False)
     voltage = db.Column("voltage", db.Float, nullable=True)
     current = db.Column("current", db.Float, nullable=True)
     temperature = db.Column("temperature", db.Float, nullable=True)
+
+    children = db.relationship("Child", secondary=association_table_soh_env)
 
     def to_dict(self):
         return {
