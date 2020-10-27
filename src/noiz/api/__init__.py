2661782bd62645f5110f5bf342085131f6263af5
Damian Kula <dkula@unistra.fr>
2020-10-27 11:12:55 +0100

    Renames argument from startdate to starttime
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/__init__.py
+++ src/noiz/api/__init__.py
@@ -1,5 +1,5 @@
 from noiz.api.component_pair import fetch_component_pairs
-from noiz.api.timespan import fetch_timespans_for_doy
+from noiz.api.timespan import fetch_timespans_for_doy, fetch_timespans_between_dates
 from noiz.api.component import fetch_components, fetch_components_by_id
 from noiz.api.datachunk import fetch_datachunks
 
