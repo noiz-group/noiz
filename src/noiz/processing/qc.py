513aaae49002129ff947aa6df6704ef34224ca59
Damian Kula <dkula@unistra.fr>
2020-11-05 20:28:33 +0100

    Do not modify dict that you are reading from
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/processing/qc.py
+++ src/noiz/processing/qc.py
@@ -21,22 +21,23 @@ def load_qc_one_config_toml(filepath: Path) -> QCOneHolder:
         raise ValueError("Provided filepath has to be a path to existing file")
 
     with open(file=filepath, mode='r') as f:
-        loaded_config = toml.load(f=f)
+        loaded_config: Dict = toml.load(f=f)  # type: ignore
 
     return validate_dict_as_qcone_holder(loaded_config)
 
 
-def validate_dict_as_qcone_holder(loaded_dict: Union[Dict, MutableMapping[str, Any]]) -> QCOneHolder:
+def validate_dict_as_qcone_holder(loaded_dict: Dict) -> QCOneHolder:
     """
     Takes a dict, or an output from TOML parser and tries to convert it into a :class:`~noiz.models.QCOneHolder` object
 
     :param loaded_dict: Dictionary to be parsed and validated as QCOneHolder
-    :type loaded_dict: Union[Dict, MutableMapping[str, Any]]
+    :type loaded_dict: Dict
     :return: Valid QCOneHolder object
     :rtype: QCOneHolder
     """
+    processed_dict = loaded_dict.copy()
     validated_forbidden_channels = []
     for forb_chn in loaded_dict['rejected_times']:
         validated_forbidden_channels.append(QCOneRejectedTimeHolder(**forb_chn))
-    loaded_dict['rejected_times'] = validated_forbidden_channels
-    return QCOneHolder(**loaded_dict)
+    processed_dict['rejected_times'] = validated_forbidden_channels
+    return QCOneHolder(**processed_dict)
