c4a4918f0f8030783f57bb535a9207697f3c8b2d
Damian Kula <dkula@unistra.fr>
2020-11-05 19:15:44 +0100

    adds method that simply adds the instance of QCOne model to DB
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/qc.py
+++ src/noiz/api/qc.py
@@ -23,6 +23,7 @@ def fetch_qc_one(ids: Union[int, Collection[int]]) -> List[QCOne]:
 def create_qcone_rejected_time(
         holder: QCOneRejectedTimeHolder,
 ) -> List[QCOneRejectedTime]:
+
     fetched_components = fetch_components(
         networks=holder.network,
         stations=holder.station,
@@ -40,7 +41,21 @@ def create_qcone_rejected_time(
 def create_qcone(
         qcone_holder: Optional[QCOneHolder] = None,
         **kwargs,
-):
+) -> QCOne:
+    """
+    This method takes a :class:`~noiz.processing.qc.QCOneHolder` instance and based on it creates an instance
+    of database model :class:`~noiz.models.QCOne`.
+
+    Optionally, it can create the instance of :class:`~noiz.processing.qc.QCOneHolder` from provided kwargs, but
+    why dont you do it on your own to ensure that it will get everything it needs?
+
+    :param qcone_holder: Object containing all required elements to create a QCOne instance
+    :type qcone_holder: QCOneHolder
+    :param kwargs: Optional kwargs to create QCOneHolder
+    :return: Working QCOne model that needs to be inserted into db
+    :rtype: QCOne
+    """
+
     if qcone_holder is None:
         qcone_holder = validate_dict_as_qcone_holder(kwargs)
 
@@ -57,3 +72,17 @@ def create_qcone(
         avg_gps_time_uncertainty_max=qcone_holder.avg_gps_time_uncertainty_max,
     )
     return qcone
+
+
+def insert_qc_one_into_db(qcone: QCOne):
+    """
+    This is method simply adding an instance of :class:`~noiz.models.QCOne` to DB and committing changes.
+
+    :param qcone: Instance of QCOne to be added to db
+    :type qcone: QCOne
+    :return: None
+    :rtype: NoneType
+    """
+    db.session.add(qcone)
+    db.session.commit()
+    return
