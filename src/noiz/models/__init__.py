91ff212a5fb3e3d7c769375b5da2605824b6a663
Damian Kula <dkula@unistra.fr>
2020-11-09 12:26:39 +0100

    Brings back ordered imports because they are vital to proper initialization of all models

--- src/noiz/models/__init__.py
+++ src/noiz/models/__init__.py
@@ -1,13 +1,13 @@
-# # Do not reorder, models are dependent on each other
-#
-# from noiz.models.timespan import Timespan
-# from noiz.models.component import Component
-# from noiz.models.processing_params import DatachunkPreprocessingConfig
-# from noiz.models.time_series_index import Tsindex
-# from noiz.models.soh import SohInstrument, SohGps
-# from noiz.models.datachunk import Datachunk,\
-#     DatachunkFile, ProcessedDatachunk, ProcessedDatachunkFile
-# from noiz.models.qc import QCOneConfig, QCOneRejectedTime, QCOneResults
-# from noiz.models.component_pair import ComponentPair
-# from noiz.models.crosscorrelation import Crosscorrelation
-# from noiz.models.stacking import CCFStack, StackingSchema, StackingTimespan
+# Do not reorder, models are dependent on each other
+
+from noiz.models.timespan import Timespan
+from noiz.models.component import Component
+from noiz.models.component_pair import ComponentPair
+from noiz.models.processing_params import DatachunkPreprocessingConfig
+from noiz.models.time_series_index import Tsindex
+from noiz.models.soh import SohInstrument, SohGps
+from noiz.models.datachunk import Datachunk,\
+    DatachunkFile, ProcessedDatachunk, ProcessedDatachunkFile
+from noiz.models.qc import QCOneConfig, QCOneRejectedTime, QCOneResults
+from noiz.models.crosscorrelation import Crosscorrelation
+from noiz.models.stacking import CCFStack, StackingSchema, StackingTimespan
