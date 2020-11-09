0535333d6e13d1f0a1e8f1bb7730968fcd520fa7
Damian Kula <dkula@unistra.fr>
2020-11-09 17:52:37 +0100

    Splits import of datachunk into two, Datachunk and ProcessedDatachunk
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/models/__init__.py
+++ src/noiz/models/__init__.py
@@ -6,8 +6,8 @@ from noiz.models.component_pair import ComponentPair
 from noiz.models.processing_params import DatachunkPreprocessingConfig
 from noiz.models.time_series_index import Tsindex
 from noiz.models.soh import SohInstrument, SohGps
-from noiz.models.datachunk import Datachunk,\
-    DatachunkFile, ProcessedDatachunk, ProcessedDatachunkFile
+from noiz.models.datachunk import Datachunk, DatachunkFile
+from noiz.models.datachunk import ProcessedDatachunk, ProcessedDatachunkFile
 from noiz.models.qc import QCOneConfig, QCOneRejectedTime, QCOneResults
 from noiz.models.crosscorrelation import Crosscorrelation
 from noiz.models.stacking import CCFStack, StackingSchema, StackingTimespan
