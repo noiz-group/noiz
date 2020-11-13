0e3c285647f3df51e3ad14f19fc5014f7c45dde7
Damian Kula <dkula@unistra.fr>
2020-11-13 15:47:12 +0100

    Adds test for adding inventory from cli
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- tests/system_tests/cli/test_data_ingestion.py
+++ tests/system_tests/cli/test_data_ingestion.py
@@ -1,10 +1,28 @@
 import pytest
 pytestmark = [pytest.mark.system, pytest.mark.cli]
 
+from click.testing import CliRunner
+from pathlib import Path
+
+from noiz.cli import cli
+
 
 @pytest.mark.system
 class TestDataIngestionRoutines:
 
+    def test_add_inventory_data(self):
+
+        inventory_path = Path(__file__).absolute().joinpath('..', 'dataset', 'STI_station_minimal.xml')
+
+        runner = CliRunner()
+        result = runner.invoke(cli, ["data", "add_inventory", str(inventory_path)])
+
+        assert result.exit_code == 0
+
     @pytest.mark.xfail
     def test_add_seismic_data(self):
+        assert False
+
+    @pytest.mark.xfail
+    def test_add_soh_data(self):
         assert False
