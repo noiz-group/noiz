e52277d54597e7f0187062d668163c1de0e081b7
Damian Kula <dkula@unistra.fr>
2020-11-17 17:34:44 +0100

    Adds unit test for that method
    Signed-off-by: Damian Kula <dkula@unistra.fr>

---
+++ tests/processing/test_validation_helpers.py
@@ -0,0 +1,18 @@
+import pytest
+import numpy as np
+
+from noiz.processing.validation_helpers import count_consecutive_trues
+
+
+@pytest.mark.parametrize(["input", "output"],
+                         [
+                             (np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0], dtype=bool), np.array([7])),
+                             (np.array([0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0], dtype=bool), np.array([3, 3])),
+                             (np.array([0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1], dtype=bool), np.array([3, 3, 1])),
+                             (np.array([1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1], dtype=bool), np.array([1, 3, 3, 1])),
+                             (np.array([1, 1, 1], dtype=bool), np.array([3])),
+                             (np.array([0, 0], dtype=bool), np.array([])),
+                             (np.array([1, 1], dtype=bool), np.array([2])),
+                         ])
+def test_count_consecutive_trues(input, output):
+    assert output == count_consecutive_trues(input)
