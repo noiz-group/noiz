60865458451ac4693ab6345923e75369578b6375
Damian Kula <dkula@unistra.fr>
2020-11-18 20:41:47 +0100

    Adds test for both ends
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- tests/processing/test_datachunk_preparation.py
+++ tests/processing/test_datachunk_preparation.py
@@ -438,7 +438,35 @@ def test_interpolate_ends_to_zero_to_fit_timespan_beginning_only():
     st[0].data *= 4
     expected_no_samples = 10
 
-    expected_data = np.array([0., 1., 2., 3., 4., 4., 4., 4., 4., 4., 4.])
+    expected_data = np.array([0., 1., 2., 3., 4., 4., 4., 4., 4., 4.])
+
+    st_res = interpolate_ends_to_zero_to_fit_timespan(st=st, timespan=ts, expected_no_samples=expected_no_samples)
+
+    assert len(st_res) == 1
+    assert len(st_res[0].data) == expected_no_samples
+    assert np.array_equal(st_res[0].data, expected_data)
+
+
+def test_interpolate_ends_to_zero_to_fit_timespan_both_ends():
+    from pandas import Timestamp
+
+    s = ['', '', '1 Trace(s) in Stream:',
+         'AA.XXX..HH2 | 2016-01-07T00:00:04.000000Z - '
+         '2016-01-07T03:00:00.000000Z | 1.0 Hz, 3 samples',
+         '', '']
+
+    ts = Timespan(
+        starttime=Timestamp('2016-01-07T00:00:00.000000Z'),
+        midtime=Timestamp('2016-01-07T00:00:05.000000Z'),
+        endtime=Timestamp('2016-01-07T00:00:10.000000Z'),
+    )
+
+    s = os.linesep.join(s)
+    st = Stream._dummy_stream_from_string(s)
+    st[0].data *= 4
+    expected_no_samples = 10
+
+    expected_data = np.array([0., 1., 2., 3., 4., 4., 4., 3., 2., 1.])
 
     st_res = interpolate_ends_to_zero_to_fit_timespan(st=st, timespan=ts, expected_no_samples=expected_no_samples)
 
