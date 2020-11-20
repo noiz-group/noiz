e59c337ec968f6e90d927238c62a739b2332ca70
Damian Kula <dkula@unistra.fr>
2020-11-20 17:55:27 +0100

    Extracts ExtendedEnum to globals
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/globals.py
+++ src/noiz/globals.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
 import os
 
 PROCESSED_DATA_DIR = os.environ.get("PROCESSED_DATA_DIR", '')
+
+
+class ExtendedEnum(Enum):
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))  # type: ignore
