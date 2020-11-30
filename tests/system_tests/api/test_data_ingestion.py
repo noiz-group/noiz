b86a413d1e7f1750c6f96003a00195a519c8c841
Damian Kula <dkula@unistra.fr>
2020-11-30 13:06:13 +0100

    Adds a test for existence of processed_data_dir
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- tests/system_tests/api/test_data_ingestion.py
+++ tests/system_tests/api/test_data_ingestion.py
@@ -1,11 +1,14 @@
 import pytest
 pytestmark = [pytest.mark.system, pytest.mark.api]
 
+from pathlib import Path
 import os
 
 
 @pytest.mark.system
 class TestDataIngestionRoutines:
+    def test_existence_of_processed_data_dir(self, noiz_app):
+        assert Path(noiz_app.noiz_config['processed_data_dir']).exists()
 
     @pytest.mark.xfail
     def test_add_seismic_data(self):
