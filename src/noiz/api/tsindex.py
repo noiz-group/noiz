8626c83a137202f3a4f2574071fcfdb113618c64
Damian Kula <dkula@unistra.fr>
2020-12-02 14:20:23 +0100

    Adds tsindex api call and scanning method
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/tsindex.py
+++ src/noiz/api/tsindex.py
@@ -1,9 +1,28 @@
 from pathlib import Path
 
+from noiz.processing.tsindex import run_mseedindex_on_passed_dir
+from flask import current_app as app
+
 
 def add_seismic_data(
         basedir: Path,
-        filename_pattern: str,
-        parallel: int,
+        current_dir: Path,
+        filename_pattern: str = "*",
 ):
-    pass
+    mseedindex_executable = app.config['MSEEDINDEX_EXECUTABLE']
+    postgres_host = app.config['POSTGRES_HOST']
+    postgres_user = app.config['POSTGRES_USER']
+    postgres_password = app.config['POSTGRES_PASSWORD']
+    postgres_db = app.config['POSTGRES_DB']
+
+    run_mseedindex_on_passed_dir(
+        basedir=basedir,
+        current_dir=current_dir,
+        filename_pattern=filename_pattern,
+        mseedindex_executable=mseedindex_executable,
+        postgres_host=postgres_host,
+        postgres_user=postgres_user,
+        postgres_password=postgres_password,
+        postgres_db=postgres_db,
+    )
+    return
