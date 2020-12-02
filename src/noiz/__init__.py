47a3472855711244e6ecf1012b8af737f663b222
Damian Kula <dkula@unistra.fr>
2020-12-02 14:27:26 +0100

    Removes logging config from the init
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/__init__.py
+++ src/noiz/__init__.py
@@ -1,11 +1,11 @@
 __version__ = "0.5.20201110"
 __author__ = "Damian Kula"
 
-from .logg import logger_config
-import logging.config
-
-logging.config.dictConfig(logger_config)
-log = logging.getLogger(__name__)
+# from .logg import logger_config
+# import logging.config
+#
+# logging.config.dictConfig(logger_config)
+# log = logging.getLogger(__name__)
 
 from . import models
 from . import api
