2ae9b0bf6bbc9ca10996675a582459b3cc2bc6c2
Damian Kula <dkula@unistra.fr>
2020-12-03 11:16:08 +0100

    Changes logging system to loguru
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/__init__.py
+++ src/noiz/__init__.py
@@ -1,13 +1,6 @@
 __version__ = "0.5.20201110"
 __author__ = "Damian Kula"
 
-import logging
-from rich.logging import RichHandler
-rich_handler = RichHandler(rich_tracebacks=True)
-log = logging.getLogger(__name__)
-log.addHandler(rich_handler)
-log.error(__name__)
-
 from . import models
 from . import api
 from . import processing
