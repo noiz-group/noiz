b88f332ea83c3d9c72b1b667ee62c13e74a1eef2
Damian Kula <dkula@unistra.fr>
2020-12-08 17:00:22 +0100

    Adds data file with some empty channels and respective test for it
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- tests/processing/soh/test_parsing.py
+++ tests/processing/soh/test_parsing.py
@@ -3,14 +3,28 @@ import pandas as pd
 import pytest
 
 from noiz.processing.soh.soh_column_names import SohType, SohInstrumentNames, load_parsing_parameters
-from noiz.processing.soh.parsing import read_single_soh_miniseed
+from noiz.processing.soh.parsing import _read_single_soh_miniseed_centaur
 
 
-def test_read_single_soh_miniseed():
+def test__read_single_soh_miniseed_centaur_all_channels_present():
     filepath = Path(__file__).parent.joinpath("data", "XX.S0001.D0.SOH_centaur-3_1479_20171125_100000.miniseed")
     parsing_params = load_parsing_parameters(
         station_type=SohInstrumentNames.CENTAUR.value, soh_type=SohType.MINISEED.value)
 
-    res = read_single_soh_miniseed(filepath=filepath, parsing_params=parsing_params)
+    res = _read_single_soh_miniseed_centaur(filepath=filepath, parsing_params=parsing_params)
 
     assert isinstance(res, pd.DataFrame)
+    assert isinstance(res.index, pd.DatetimeIndex)
+    assert res.shape == (2, 9)
+
+
+def test__read_single_soh_miniseed_centaur_empty_channels():
+    filepath = Path(__file__).parent.joinpath("data", "SI.SI09.D0.SOH_centaur-3_1492_20180616_040000.miniseed")
+    parsing_params = load_parsing_parameters(
+        station_type=SohInstrumentNames.CENTAUR.value, soh_type=SohType.MINISEED.value)
+
+    res = _read_single_soh_miniseed_centaur(filepath=filepath, parsing_params=parsing_params)
+
+    assert isinstance(res, pd.DataFrame)
+    assert isinstance(res.index, pd.DatetimeIndex)
+    assert res.shape == (2, 9)
