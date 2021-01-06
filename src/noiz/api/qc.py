c54cf30484f4e7ff6d18fc0479ce32d73cb41588
Damian Kula <dkula@unistra.fr>
2021-01-06 17:40:33 +0100

    moves non api methods out of api to processing
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/qc.py
+++ src/noiz/api/qc.py
@@ -1,12 +1,10 @@
 import datetime
 from loguru import logger
-import operator as ope
 from pathlib import Path
 
-from noiz.models.component import Component
 from sqlalchemy import and_
 from sqlalchemy.dialects.postgresql import insert
-from typing import List, Collection, Union, Optional, Tuple, Any, Callable
+from typing import List, Collection, Union, Optional, Tuple
 
 from noiz.api.component import fetch_components
 from noiz.api.datachunk import _determine_filters_and_opts_for_datachunk
@@ -17,9 +15,9 @@ from noiz.exceptions import EmptyResultException
 from noiz.models.datachunk import Datachunk, DatachunkStats
 from noiz.models.qc import QCOneConfig, QCOneRejectedTime, QCOneRejectedTimeHolder, QCOneHolder, QCOneResults
 from noiz.models.soh import AveragedSohGps
-from noiz.models.timespan import Timespan
 from noiz.processing.configs import validate_dict_as_qcone_holder, load_qc_one_config_toml, parse_single_config_toml, \
     DefinedConfigs
+from noiz.processing.qc import calculate_qcone_results
 
 
 def fetch_qc_ones(ids: Union[int, Collection[int]]) -> List[QCOneConfig]:
@@ -418,191 +416,6 @@ def add_or_upsert_qcone_results_in_db(qcone_results_collection: Collection[QCOne
     logger.debug('Commiting session.')
     db.session.commit()
     return
-
-
-def calculate_qcone_results(
-        datachunk: Datachunk,
-        qcone_config: QCOneConfig,
-        stats: DatachunkStats,
-        avg_soh_gps: Optional[AveragedSohGps],
-) -> QCOneResults:
-    """
-    Performs all checks of the QCOne step. It compares values in the :class:`noiz.models.datachunk.DatachunkStats`
-     and :class:`noiz.models.soh.AveragedSohGps` against :class:`noiz.models.qc.QCOneConfig` and saves the values in
-     :class:`noiz.models.qc.QCOneResults` instance that can be added to db afterwards.
-
-    :param datachunk: Datachunk to be compared
-    :type datachunk: Datachunk
-    :param qcone_config: QCOneConfig to have reference values to compare against
-    :type qcone_config: QCOneConfig
-    :param stats: Statistics of the provided Datachunk
-    :type stats: DatachunkStats
-    :param avg_soh_gps: Object with values of AveragedSohGps data
-    :type avg_soh_gps: Optional[AveragedSohGps]
-    :return: Object containing values of all performed comparisons
-    :rtype: QCOneResults
-    """
-    qcone_res = QCOneResults(datachunk_id=datachunk.id, qcone_config_id=qcone_config.id)
-
-    qcone_res = _determine_qcone_time(results=qcone_res,  datachunk=datachunk, config=qcone_config)
-    qcone_res = _determine_qcone_accepted_times(results=qcone_res, datachunk=datachunk, config=qcone_config)
-    qcone_res = _determine_qcone_gps(result=qcone_res, config=qcone_config, avg_soh_gps=avg_soh_gps)
-    qcone_res = _determine_qcone_stats(results=qcone_res, stats=stats, config=qcone_config)
-
-    return qcone_res
-
-
-def _determine_qcone_time(
-        results: QCOneResults,
-        datachunk: Datachunk,
-        config: QCOneConfig,
-) -> QCOneResults:
-    """
-    filldocs
-    """
-
-    if not isinstance(datachunk.timespan, Timespan):
-        raise ValueError('You should load timespan together with the Datachunk.')
-
-    results.starttime = compare_vals_null_safe(
-        config.starttime, datachunk.timespan.starttime, ope.le, null_value=config.null_value)
-    results.endtime = compare_vals_null_safe(
-        config.endtime, datachunk.timespan.endtime, ope.ge, null_value=config.null_value)
-
-    return results
-
-
-def _determine_qcone_accepted_times(
-        results: QCOneResults,
-        datachunk: Datachunk,
-        config: QCOneConfig,
-) -> QCOneResults:
-    """
-    filldocs
-    """
-
-    if not isinstance(datachunk.timespan, Timespan):
-        raise ValueError('You should load timespan together with the Datachunk.')
-
-    reject_checks = [True, ]
-
-    if datachunk.component_id in config.component_ids_rejected_times:
-        for rej in config.time_periods_rejected:
-            if not rej.component_id == datachunk.component_id:
-                continue
-
-            reject_checks.append(
-                    (rej.starttime <= datachunk.timespan.endtime) and (datachunk.timespan.starttime <= rej.endtime)
-            )
-            # This check is adaptation of https://stackoverflow.com/a/13513973/4308541
-
-    results.accepted_time = all(reject_checks)
-
-    return results
-
-
-def _determine_qcone_stats(
-        results: QCOneResults,
-        stats: DatachunkStats,
-        config: QCOneConfig,
-) -> QCOneResults:
-    """
-    filldocs
-    """
-
-    results.signal_energy_max = compare_vals_null_safe(
-        config.signal_energy_max, stats.energy, ope.le, config.null_value)
-    results.signal_energy_min = compare_vals_null_safe(
-        config.signal_energy_min, stats.energy, ope.ge, config.null_value)
-    results.signal_min_value_max = compare_vals_null_safe(
-        config.signal_min_value_max, stats.min, ope.le, config.null_value)
-    results.signal_min_value_min = compare_vals_null_safe(
-        config.signal_min_value_min, stats.min, ope.ge, config.null_value)
-    results.signal_max_value_max = compare_vals_null_safe(
-        config.signal_max_value_max, stats.max, ope.le, config.null_value)
-    results.signal_max_value_min = compare_vals_null_safe(
-        config.signal_max_value_min, stats.max, ope.ge, config.null_value)
-    results.signal_mean_value_max = compare_vals_null_safe(
-        config.signal_mean_value_max, stats.mean, ope.le, config.null_value)
-    results.signal_mean_value_min = compare_vals_null_safe(
-        config.signal_mean_value_min, stats.mean, ope.ge, config.null_value)
-    results.signal_variance_max = compare_vals_null_safe(
-        config.signal_variance_max, stats.variance, ope.le, config.null_value)
-    results.signal_variance_min = compare_vals_null_safe(
-        config.signal_variance_min, stats.variance, ope.ge, config.null_value)
-    results.signal_skewness_max = compare_vals_null_safe(
-        config.signal_skewness_max, stats.skewness, ope.le, config.null_value)
-    results.signal_skewness_min = compare_vals_null_safe(
-        config.signal_skewness_min, stats.skewness, ope.ge, config.null_value)
-    results.signal_kurtosis_max = compare_vals_null_safe(
-        config.signal_kurtosis_max, stats.kurtosis, ope.le, config.null_value)
-    results.signal_kurtosis_min = compare_vals_null_safe(
-        config.signal_kurtosis_min, stats.kurtosis, ope.ge, config.null_value)
-
-    return results
-
-
-def _determine_qcone_gps(
-        result: QCOneResults,
-        config: QCOneConfig,
-        avg_soh_gps: Optional[AveragedSohGps]
-) -> QCOneResults:
-    """
-    Compares values of provided instance of :class:`noiz.models.soh.AveragedSohGps` with values defined in
-    :class:`noiz.models.qc.QCOneConfig`. If as :paramref:`noiz.api.qc.determine_qcone_gps.avg_soh_gps` will me provided
-    None, all values will be set to the :py:attr:`noiz.models.qc.QCOneConfig.null_value`.
-    If any of the config or real data values will also be None, the result of comparison will be set to
-    :py:attr:`noiz.models.qc.QCOneConfig.null_value`.
-
-    :param result: Object to which the results of comparisons will be saved
-    :type result: QCOneResults,
-    :param config: Object to take the reference values to compare against
-    :type config: QCOneConfig,
-    :param avg_soh_gps: Real data values that will be used in comparison
-    :type avg_soh_gps: Optional[AveragedSohGps]
-    :return: Object that will include results of the comparison
-    :rtype: QCOneResults
-    """
-
-    if avg_soh_gps is not None:
-        result.avg_gps_time_error_max = compare_vals_null_safe(
-            config.avg_gps_time_error_max, avg_soh_gps.time_error, ope.ge, config.null_value)
-        result.avg_gps_time_error_min = compare_vals_null_safe(
-            config.avg_gps_time_error_min, avg_soh_gps.time_error, ope.le, config.null_value)
-        result.avg_gps_time_uncertainty_max = compare_vals_null_safe(
-            config.avg_gps_time_uncertainty_max, avg_soh_gps.time_uncertainty, ope.ge, config.null_value)
-        result.avg_gps_time_uncertainty_min = compare_vals_null_safe(
-            config.avg_gps_time_uncertainty_min, avg_soh_gps.time_uncertainty, ope.le, config.null_value)
-    else:
-        result.avg_gps_time_error_max = config.null_value
-        result.avg_gps_time_error_min = config.null_value
-        result.avg_gps_time_uncertainty_max = config.null_value
-        result.avg_gps_time_uncertainty_min = config.null_value
-    return result
-
-
-def compare_vals_null_safe(a: Any, b: Any, op: Callable[[Any, Any], bool], null_value: bool):
-    """
-    Compares two values with provided callable. Callable, should be coming from the :py:mod:`operator`.
-    It first checks if any of provided values is None and if yes, returns a provided
-    :paramref:`noiz.api.qc.compare_vals_null_safe.null_value`.
-
-    :param a: First value to compare
-    :type a: Any
-    :param b: Second value to compare
-    :type b: Any
-    :param op: Callable to perform comparison with.
-    :type op: Callable[[Any, Any], bool]
-    :param null_value:
-    :type null_value: bool
-    :return: Returns result of a call or a value of :paramref:`noiz.api.qc.compare_vals_null_safe.null_value`
-    :rtype: bool
-    """
-
-    if a is None or b is None:
-        return null_value
-    else:
-        return op(a, b)
 
 
 def insert_qconeconfig_into_db(params: QCOneConfig) -> None:
