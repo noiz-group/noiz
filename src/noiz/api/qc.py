f9d8bf0ef30aa3e58b2770a01c77e5d563b5f4ec
Damian Kula <dkula@unistra.fr>
2021-01-06 11:52:26 +0100

    Docstrings
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/qc.py
+++ src/noiz/api/qc.py
@@ -433,6 +433,23 @@ def determine_qcone_gps(
         config: QCOneConfig,
         avg_soh_gps: Optional[AveragedSohGps]
 ) -> QCOneResults:
+    """
+    Compares values of provided instance of :class:`noiz.models.soh.AveragedSohGps` with values defined in
+    :class:`noiz.models.qc.QCOneConfig`. If as :paramref:`noiz.api.qc.determine_qcone_gps.avg_soh_gps` will me provided
+    None, all values will be set to the :py:attr:`noiz.models.qc.QCOneConfig.null_value`.
+    If any of the config or real data values will also be None, the result of comparison will be set to
+    :py:attr:`noiz.models.qc.QCOneConfig.null_value`.
+
+    :param result: Object to which the results of comparisons will be saved
+    :type result: QCOneResults,
+    :param config: Object to take the reference values to compare against
+    :type config: QCOneConfig,
+    :param avg_soh_gps: Real data values that will be used in comparison
+    :type avg_soh_gps: Optional[AveragedSohGps]
+    :return: Object that will include results of the comparison
+    :rtype: QCOneResults
+    """
+
     if avg_soh_gps is not None:
         result.avg_gps_time_error_max = compare_vals_null_safe(
             config.avg_gps_time_error_max, avg_soh_gps.time_error, ope.ge, config.null_value)
@@ -451,6 +468,23 @@ def determine_qcone_gps(
 
 
 def compare_vals_null_safe(a: Any, b: Any, op: Callable[[Any, Any], bool], null_value: bool):
+    """
+    Compares two values with provided callable. Callable, should be coming from the :py:mod:`operator`.
+    It first checks if any of provided values is None and if yes, returns a provided
+    :paramref:`noiz.api.qc.compare_vals_null_safe.null_value`.
+
+    :param a: First value to compare
+    :type a: Any
+    :param b: Second value to compare
+    :type b: Any
+    :param op: Callable to perform comparison with.
+    :type op: Callable[[Any, Any], bool]
+    :param null_value:
+    :type null_value: bool
+    :return: Returns result of a call or a value of :paramref:`noiz.api.qc.compare_vals_null_safe.null_value`
+    :rtype: bool
+    """
+
     if a is None or b is None:
         return null_value
     else:
