4e039072b01cb176206401963dae30da3da0e089
Damian Kula <dkula@unistra.fr>
2021-01-07 15:19:43 +0100

    Adds fetcher for the ProcessedDatachunkParams
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/processing_config.py
+++ src/noiz/api/processing_config.py
@@ -1,10 +1,9 @@
 from loguru import logger
-
-from noiz.models.qc import QCOneConfig, QCOneHolder
 from pathlib import Path
 from typing import Optional, Tuple, Union
 
 from noiz.database import db
+from noiz.exceptions import EmptyResultException
 from noiz.models.processing_params import DatachunkParams, DatachunkParamsHolder, ProcessedDatachunkParams, \
     ProcessedDatachunkParamsHolder
 from noiz.processing.configs import validate_config_dict_as_datachunkparams, parse_single_config_toml, DefinedConfigs, \
@@ -23,6 +22,21 @@ def fetch_datachunkparams_by_id(id: int) -> DatachunkParams:
     if fetched_params is None:
         raise ValueError(f"DatachunkParams object of id {id} does not exist.")
 
+    return fetched_params
+
+
+def fetch_processed_datachunk_params_by_id(id: int) -> ProcessedDatachunkParams:
+    """
+    Fetches a ProcessedDatachunkParams objects by its ID.
+    :param id: ID of processing params to be fetched
+    :type id: int
+    :return: fetched ProcessedDatachunkParams object
+    :rtype: ProcessedDatachunkParams
+    :raises ValueError
+    """
+    fetched_params = ProcessedDatachunkParams.query.filter_by(id=id).first()
+    if fetched_params is None:
+        raise EmptyResultException(f"ProcessedDatachunkParams object of id {id} does not exist.")
     return fetched_params
 
 
@@ -98,12 +112,13 @@ def create_processed_datachunk_params(
 
 def insert_params_into_db(params: Union[DatachunkParams, ProcessedDatachunkParams]):
     """
-    This is method simply adding an instance of :class:`~noiz.models.DatachunkParams` to DB and committing changes.
+    This is method simply adding an instance of :py:class:`~noiz.models.DatachunkParams`
+    or :py:class:`~noiz.models.ProcessedDatachunkParams` to DB and committing changes.
 
     Has to be executed within `app_context`
 
-    :param params: Instance of DatachunkParams to be added to db
-    :type params: DatachunkParams
+    :param params: Instance of supported params object to be added to db
+    :type params: Union[DatachunkParams, ProcessedDatachunkParams]
     :return: None
     :rtype: NoneType
     """
@@ -119,6 +134,7 @@ def create_and_add_datachunk_params_config_from_toml(
         add_to_db: bool = False
 ) -> Optional[Tuple[DatachunkParamsHolder, DatachunkParams]]:
     """
+    filldocs
     This method takes a filepath to a TOML file with valid parameters
     to create a :class:`~noiz.processing.qc.QCOneHolder` and subsequently :class:`~noiz.models.QCOneConfig`.
     It can also add the created object to the database. By default it does not add it to db.
@@ -130,7 +146,7 @@ def create_and_add_datachunk_params_config_from_toml(
     :param add_to_db: If the result of parsing of TOML should be added to DB
     :type add_to_db: bool
     :return: It can return QCOneHolder object for manual validation
-    :rtype: Optional[QCOneHolder]
+    :rtype: Optional[Tuple[DatachunkParamsHolder, DatachunkParams]]
     """
 
     params_holder = parse_single_config_toml(filepath=filepath, config_type=DefinedConfigs.DATACHUNKPARAMS)
@@ -148,18 +164,7 @@ def create_and_add_processed_datachunk_params_from_toml(
         add_to_db: bool = False
 ) -> Optional[Tuple[ProcessedDatachunkParamsHolder, ProcessedDatachunkParams]]:
     """
-    This method takes a filepath to a TOML file with valid parameters
-    to create a :class:`~noiz.processing.qc.QCOneHolder` and subsequently :class:`~noiz.models.QCOneConfig`.
-    It can also add the created object to the database. By default it does not add it to db.
-    If chosen not to add the result to db, a tuple containing both :class:`~noiz.processing.qc.QCOneHolder`
-    and :class:`~noiz.models.QCOneConfig` will be returned for manual check.
-
-    :param filepath: Path to existing TOML file
-    :type filepath: Path
-    :param add_to_db: If the result of parsing of TOML should be added to DB
-    :type add_to_db: bool
-    :return: It can return QCOneHolder object for manual validation
-    :rtype: Optional[QCOneHolder]
+    filldocs
     """
 
     params_holder = parse_single_config_toml(filepath=filepath, config_type=DefinedConfigs.PROCESSEDDATACHUNKPARAMS)
