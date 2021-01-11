21443a2f7262c014110298feaff578cdefdb28e9
Damian Kula <dkula@unistra.fr>
2021-01-11 19:39:18 +0100

    Scratch of new method for cc
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/crosscorrelations.py
+++ src/noiz/api/crosscorrelations.py
@@ -1,11 +1,15 @@
 import datetime
 from loguru import logger
+from noiz.api.helpers import extract_object_ids
+
+from noiz.api.timespan import fetch_timespans_between_dates
+
 from obspy.signal.cross_correlation import correlate
 from sqlalchemy import and_
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import subqueryload
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Union
 
 from noiz.database import db
 from noiz.models.component import Component
@@ -82,6 +86,142 @@ def perform_crosscorrelations_for_day_and_pairs(
     processing_params_id=1,
     bulk_insert=True,
 ):
+
+    components_to_correlate = split_component_pairs_to_components(pairs_to_correlate)
+    year, day_of_year = get_year_doy(execution_date)
+
+    processing_params = (
+        db.session.query(DatachunkParams)
+        .filter(DatachunkParams.id == processing_params_id)
+        .first()
+    )
+
+    logger.info(
+        f"Querrying for components that are present on day {year}.{day_of_year}"
+    )
+
+    components_day = (
+        db.session.query(Timespan, Component)
+        .join(Datachunk)
+        .join(ProcessedDatachunk)
+        .join(Component)
+        .distinct(Component.id)
+        .filter(
+            Timespan.starttime_year == year,
+            Timespan.starttime_doy == day_of_year,
+            Component.component.in_(components_to_correlate),
+        )
+        .all()
+    )
+
+    components_day = [cmp for _, cmp in components_day]
+    component_ids = [cmp.id for cmp in components_day]
+    logger.info(f"There are {len(component_ids)} unique components")
+
+    component_pairs_day = (
+        db.session.query(
+            ComponentPair, ComponentPair.component_a_id, ComponentPair.component_b_id
+        )
+        .options(
+            subqueryload(ComponentPair.component_a),
+            subqueryload(ComponentPair.component_b),
+        )
+        .filter(
+            and_(
+                and_(
+                    ComponentPair.component_a_id.in_(component_ids),
+                    ComponentPair.component_b_id.in_(component_ids),
+                ),
+                ComponentPair.autocorrelation == autocorrelations,
+                ComponentPair.intracorrelation == intrastation_correlations,
+                ComponentPair.component_names.in_(pairs_to_correlate),
+            )
+        )
+    ).all()
+    logger.info(
+        f"There are {len(component_pairs_day)} component pairs to be correlated that day"
+    )
+
+    groupped_componentpairs = group_componentpairs_by_componenta_componentb(
+        component_pairs_day
+    )
+
+    logger.info("Looking for all processed datachunks for that day")
+    processed_datachunks_day = fetch_processeddatachunks_a_day(date=execution_date)
+    logger.info(
+        f"There are {len(processed_datachunks_day)} processed_datachunks available for {execution_date.date()}"
+    )
+    groupped_chunks = group_chunks_by_timespanid_componentid(processed_datachunks_day)
+
+    no_timespans = len(groupped_chunks)
+    logger.info(
+        f"Groupping all_possible correlations. There are {no_timespans} timespans to check."
+    )
+    xcorrs = []
+    for i, (timespan, chunks) in enumerate(groupped_chunks.items()):
+        logger.info(f"Starting to look for correlations in {i + 1}/{no_timespans}")
+        timespan_corrs = find_correlations_in_chunks(chunks, groupped_componentpairs)
+        logger.info("Loading data for that timespan")
+        streams = load_data_for_chunks(chunks)
+
+        no_corrs = len(timespan_corrs)
+        logger.info(f"Starting correlation of data. There are {no_corrs} to do")
+
+        for cmp_a, components_b in timespan_corrs.items():
+            for cmp_b, current_pair in components_b.items():
+                if streams[cmp_a][0].data.shape != streams[cmp_b][0].data.shape:
+                    logger.error(
+                        f"The shapes of data arrays for {cmp_a} and {cmp_b} are different. "
+                        f"Shapes: {cmp_a} is {streams[cmp_a][0].data.shape} "
+                        f"{cmp_b} is {streams[cmp_b][0].data.shape} "
+                    )
+
+                ccf_data = correlate(
+                    a=streams[cmp_a][0],
+                    b=streams[cmp_b][0],
+                    shift=processing_params.get_correlation_max_lag_samples(),
+                )
+
+                xcorr = Crosscorrelation(
+                    processing_params_id=processing_params.id,
+                    componentpair_id=current_pair.id,
+                    timespan_id=timespan,
+                    ccf=ccf_data,
+                )
+                xcorrs.append(xcorr)
+
+        logger.info(f"Correlations for timespan {timespan} done")
+
+    if bulk_insert:
+        logger.info("Trying to do bulk insert")
+        try:
+            bulk_add_crosscorrelations(xcorrs)
+        except IntegrityError as e:
+            logger.warning(f"There was an integrity error thrown. {e}")
+            logger.warning("Rollingback session")
+            db.session.rollback()
+            logger.warning("Retrying with upsert")
+            upsert_crosscorrelations(xcorrs)
+    else:
+        logger.info(
+            f"Starting to perform careful upsert. There are {len(xcorrs)} to insert"
+        )
+        upsert_crosscorrelations(xcorrs)
+
+    logger.info("Success!")
+    return
+
+def perform_crosscorrelations(
+    starttime: Union[datetime.date, datetime.datetime],
+    endtime: Union[datetime.date, datetime.datetime],
+    pairs_to_correlate=("ZZ",),
+    autocorrelations=False,
+    intrastation_correlations=False,
+    processing_params_id=1,
+    bulk_insert=True,
+):
+    fetched_timespans = fetch_timespans_between_dates(starttime=starttime, endtime=endtime)
+    fetched_timespans_ids = extract_object_ids(fetched_timespans)
 
     components_to_correlate = split_component_pairs_to_components(pairs_to_correlate)
     year, day_of_year = get_year_doy(execution_date)
