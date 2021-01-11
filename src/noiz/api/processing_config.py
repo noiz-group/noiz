da6c838c9564f87f2b03eeff3c7ff0e528a866ac
Damian Kula <dkula@unistra.fr>
2021-01-11 18:25:16 +0100

    Adds processing and api calls to create crosscorrelation_params
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/api/processing_config.py
+++ src/noiz/api/processing_config.py
@@ -5,14 +5,15 @@ from typing import Optional, Tuple, Union
 from noiz.database import db
 from noiz.exceptions import EmptyResultException
 from noiz.models.processing_params import DatachunkParams, DatachunkParamsHolder, ProcessedDatachunkParams, \
-    ProcessedDatachunkParamsHolder
+    ProcessedDatachunkParamsHolder, CrosscorrelationParams, CrosscorrelationParamsHolder
 from noiz.processing.configs import parse_single_config_toml, DefinedConfigs, \
-    create_datachunkparams, create_processed_datachunk_params
+    create_datachunkparams, create_processed_datachunk_params, create_crosscorrelation_params
 
 
 def fetch_datachunkparams_by_id(id: int) -> DatachunkParams:
     """
     Fetches a DatachunkParams objects by its ID.
+
     :param id: ID of processing params to be fetched
     :type id: int
     :return: fetched DatachunkParams object
@@ -28,6 +29,7 @@ def fetch_datachunkparams_by_id(id: int) -> DatachunkParams:
 def fetch_processed_datachunk_params_by_id(id: int) -> ProcessedDatachunkParams:
     """
     Fetches a ProcessedDatachunkParams objects by its ID.
+
     :param id: ID of processing params to be fetched
     :type id: int
     :return: fetched ProcessedDatachunkParams object
@@ -40,15 +42,32 @@ def fetch_processed_datachunk_params_by_id(id: int) -> ProcessedDatachunkParams:
     return fetched_params
 
 
-def _insert_params_into_db(params: Union[DatachunkParams, ProcessedDatachunkParams]):
+def fetch_crosscorrelation_params_by_id(id: int) -> CrosscorrelationParams:
     """
-    This is method simply adding an instance of :py:class:`~noiz.models.DatachunkParams`
-    or :py:class:`~noiz.models.ProcessedDatachunkParams` to DB and committing changes.
+    Fetches a CrosscorrelationParams objects by its ID.
+
+    :param id: ID of processing params to be fetched
+    :type id: int
+    :return: fetched CrosscorrelationParams object
+    :rtype: CrosscorrelationParams
+    :raises ValueError
+    """
+    fetched_params = CrosscorrelationParams.query.filter_by(id=id).first()
+    if fetched_params is None:
+        raise EmptyResultException(f"CrosscorrelationParams object of id {id} does not exist.")
+    return fetched_params
+
+
+def _insert_params_into_db(params: Union[DatachunkParams, ProcessedDatachunkParams, CrosscorrelationParams]):
+    """
+    This is method simply adding an instance of :py:class:`~noiz.models.DatachunkParams`,
+    :py:class:`~noiz.models.ProcessedDatachunkParams` or :py:class:`~noiz.models.CrosscorrelationParams`
+    to DB and committing changes.
 
     Has to be executed within `app_context`
 
     :param params: Instance of supported params object to be added to db
-    :type params: Union[DatachunkParams, ProcessedDatachunkParams]
+    :type params: Union[DatachunkParams, ProcessedDatachunkParams, EmptyResultException]
     :return: None
     :rtype: NoneType
     """
@@ -98,10 +117,45 @@ def create_and_add_processed_datachunk_params_from_toml(
     """
 
     params_holder = parse_single_config_toml(filepath=filepath, config_type=DefinedConfigs.PROCESSEDDATACHUNKPARAMS)
-    datachunk_params = create_processed_datachunk_params(params_holder=params_holder)
+    try:
+        _ = fetch_datachunkparams_by_id(
+            id=params_holder.datachunk_params_id
+        )
+    except EmptyResultException:
+        raise EmptyResultException(f"There are no processed_datachunk_params in the database with requested id: "
+                                   f"{params_holder.datachunk_params_id}")
+
+    params = create_processed_datachunk_params(params_holder=params_holder)
 
     if add_to_db:
-        _insert_params_into_db(params=datachunk_params)
+        _insert_params_into_db(params=params)
     else:
-        return (params_holder, datachunk_params)
+        return (params_holder, params)
+    return None
+
+
+def create_and_add_crosscorrelation_params_from_toml(
+        filepath: Path,
+        add_to_db: bool = False
+) -> Optional[Tuple[CrosscorrelationParamsHolder, CrosscorrelationParams]]:
+    """
+    filldocs
+    """
+
+    params_holder = parse_single_config_toml(filepath=filepath, config_type=DefinedConfigs.CROSSCORRELATIONPARAMS)
+
+    try:
+        processed_datachunk_params = fetch_processed_datachunk_params_by_id(
+            id=params_holder.processed_datachunk_params_id
+        )
+    except EmptyResultException:
+        raise EmptyResultException(f"There are no processed_datachunk_params in the database with requested id: "
+                                   f"{params_holder.processed_datachunk_params_id}")
+
+    params = create_crosscorrelation_params(params_holder=params_holder, processed_params=processed_datachunk_params)
+
+    if add_to_db:
+        _insert_params_into_db(params=params)
+    else:
+        return (params_holder, params)
     return None
