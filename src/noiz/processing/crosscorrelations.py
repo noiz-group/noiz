e5bfaf470fb0f10c5512bfde52e2fa06f9200038
Damian Kula <dkula@unistra.fr>
2021-01-12 19:04:36 +0100

    Removes dead methods
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/processing/crosscorrelations.py
+++ src/noiz/processing/crosscorrelations.py
@@ -1,10 +1,9 @@
 from collections import defaultdict
 import numpy as np
 import obspy
-from typing import Tuple, Iterable, Dict, DefaultDict, Collection
+from typing import Tuple, Dict, DefaultDict, Collection
 
 from noiz.exceptions import CorruptedDataException
-from noiz.models.component_pair import ComponentPair
 from noiz.models.datachunk import ProcessedDatachunk
 
 
@@ -12,6 +11,7 @@ def get_time_vector_ccf(max_lag: float, sampling_rate: float) -> np.array:
     """
     Calculate a timeseries symmetric around 0 with given max timelag and sampling rate.
     Length of the timeseries will be 2*max_lag*sampling_rate + 1
+
     :param max_lag: Maximum timelag in seconds
     :type max_lag: float
     :param sampling_rate: sampling rate in Hz
@@ -43,57 +43,6 @@ def group_chunks_by_timespanid_componentid(
     for chunk in processed_datachunks:
         groupped_chunks[chunk.datachunk.timespan_id][chunk.datachunk.component_id] = chunk
     return groupped_chunks
-
-
-def group_componentpairs_by_componenta_componentb(
-    component_pairs: Iterable[Tuple[ComponentPair, int, int]]
-) -> DefaultDict[int, Dict[int, ComponentPair]]:
-    """
-    Groups provided component pairs by first, ID of a first pair and then by id of a second pair.
-
-    :param component_pairs: Iterable of tuples with ComponentPair and ids of both component
-    :type component_pairs: Iterable[Tuple[ComponentPair, int, int]]
-    :return: Dict with component pairs grouped by first component id and then second component id
-    :rtype: DefaultDict[int, Dict[int, ComponentPair]]
-    """
-    groupped_componentpairs: DefaultDict[int, Dict[int, ComponentPair]] = defaultdict(
-        dict
-    )
-    for component_pair, component_a_id, component_b_id in component_pairs:
-        groupped_componentpairs[component_a_id][component_b_id] = component_pair
-    return groupped_componentpairs
-
-
-def find_correlations_in_chunks(
-    chunks: Dict[int, ProcessedDatachunk],
-    groupped_componentpairs: DefaultDict[int, Dict[int, ComponentPair]],
-) -> DefaultDict[int, Dict[int, ComponentPair]]:
-
-    local_corrs: DefaultDict[int, Dict[int, ComponentPair]] = defaultdict(dict)
-    for component_a_id, chunk_a in chunks.items():
-
-        group_component_a = groupped_componentpairs.get(component_a_id)
-        if group_component_a is None:
-            continue
-
-        for component_b_id, chunk_b in chunks.items():
-            found_pair = group_component_a.get(component_b_id)
-
-            if found_pair is None:
-                continue
-            else:
-                if (
-                    found_pair.component_a == chunk_a.datachunk.component
-                    and found_pair.component_b == chunk_b.datachunk.component
-                ):
-                    local_corrs[component_a_id][component_b_id] = found_pair
-                else:
-                    raise ValueError(
-                        f"The found correlation has wrong relations with chunks! \
-                                     Was expecting on `a`: {found_pair.component_a} got {chunk_a.datachunk.component};\
-                                     on `b`: {found_pair.component_b} got {chunk_b.datachunk.component}"
-                    )
-    return local_corrs
 
 
 def load_data_for_chunks(
