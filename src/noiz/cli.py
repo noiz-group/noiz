5c784f51dc921665e7ab8a28ed101df387e31410
Damian Kula <dkula@unistra.fr>
2021-01-20 18:33:38 +0100

    Adds a cli call for adding qctwoconfig
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/cli.py
+++ src/noiz/cli.py
@@ -152,11 +152,33 @@ def add_qcone_config(
         click.echo(parsing_results)
 
 
+@configs_group.command("add_qctwo_config")
+@with_appcontext
+@click.option("-f", "--filepath", nargs=1, type=click.Path(exists=True), required=True)
+@click.option('--add_to_db', is_flag=True, expose_value=True,
+              prompt='Are you sure you want to add QCTwoConfig to DB? `N` will just preview it. ')
+def add_qctwo_config(
+        filepath: str,
+        add_to_db: bool,
+):
+    """Read a TOML file with QCTwoConfig and add to db."""
+
+    from noiz.api.processing_config import create_and_add_qctwo_config_from_toml
+
+    if add_to_db:
+        params = create_and_add_qctwo_config_from_toml(filepath=Path(filepath), add_to_db=add_to_db)
+        click.echo(f"The QCTwoConfig was added to db with id {params.id}")
+    else:
+        parsing_results, _ = create_and_add_qctwo_config_from_toml(filepath=Path(filepath), add_to_db=add_to_db)
+        click.echo("\n")
+        click.echo(parsing_results)
+
+
 @configs_group.command("add_stacking_schema")
 @with_appcontext
 @click.option("-f", "--filepath", nargs=1, type=click.Path(exists=True), required=True)
 @click.option('--add_to_db', is_flag=True, expose_value=True,
-              prompt='Are you sure you want to add QCOneConfig to DB? `N` will just preview it. ')
+              prompt='Are you sure you want to add StackingSchema to DB? `N` will just preview it. ')
 def add_stacking_schema(
         filepath: str,
         add_to_db: bool,
