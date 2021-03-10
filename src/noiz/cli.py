f738c3076c65ea83f0b57b5c94a8c251281e53b3
Damian Kula <dkula@unistra.fr>
2021-03-10 18:27:35 +0100

    Adds Cli call, removes unnecessary argument from method
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/cli.py
+++ src/noiz/cli.py
@@ -501,6 +501,42 @@ def run_qcone(
     )
 
 
+@processing_group.command("run_beamforming")
+@with_appcontext
+@click.option("-s", "--station", multiple=True, type=str, callback=_validate_zero_length_as_none)
+@click.option("-c", "--component", multiple=True, type=str, callback=_validate_zero_length_as_none)
+@click.option("-sd", "--startdate", nargs=1, type=str, required=True, callback=_parse_as_date)
+@click.option("-ed", "--enddate", nargs=1, type=str, required=True, callback=_parse_as_date)
+@click.option("-p", "--beamforming_params_id", nargs=1, type=int,
+              default=1, show_default=True)
+@click.option("-b", "--batch_size", nargs=1, type=int, default=1000, show_default=True)
+@click.option('--parallel/--no_parallel', default=True)
+@click.option('--skip_existing/--no_skip_existing', default=True)
+def run_beamforming(
+        station,
+        component,
+        startdate,
+        enddate,
+        beamforming_params_id,
+        batch_size,
+        parallel,
+        skip_existing,
+):
+    """Start performing beamforming"""
+
+    from noiz.api.beamforming import run_beamforming
+    run_beamforming(
+        stations=station,
+        components=component,
+        starttime=startdate,
+        endtime=enddate,
+        beamforming_params_id=beamforming_params_id,
+        batch_size=batch_size,
+        parallel=parallel,
+        skip_existing=skip_existing,
+    )
+
+
 @processing_group.command("process_datachunks")
 @with_appcontext
 @click.option("-s", "--station", multiple=True, type=str, callback=_validate_zero_length_as_none)
