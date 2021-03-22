1fbf5480847b93740500d18b49bf6718a447b60e
Damian Kula <dkula@unistra.fr>
2021-03-22 14:56:07 +0100

    Adds base model for File models
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/models/ppsd.py
+++ src/noiz/models/ppsd.py
@@ -1,8 +1,67 @@
+from typing import Union
+
 from pathlib import Path
 
 from noiz.database import db
 from noiz.exceptions import MissingDataFileException
 
+from noiz.globals import PROCESSED_DATA_DIR
+from noiz.models import Timespan, Component, ComponentPair
+from noiz.models.processing_params import PPSDParams
+
+ConfigLike = Union[
+    PPSDParams,
+]
+
+class FileModel(db.Model):
+    __abstract__ = True
+    id: int = db.Column("id", db.BigInteger, primary_key=True)
+    _filepath: str = db.Column("filepath", db.UnicodeText, nullable=False)
+
+    _file_model_type: str
+
+    def __init__(self, **kwargs):
+        super(FileModel, self).__init__(**kwargs)
+
+    @property
+    def file_model_type(self):
+        return self._file_model_type
+
+    def _find_new_dipath(
+            self,
+            config: ConfigLike,
+            ts: Timespan,
+            cmp: Union[Component, ComponentPair]
+    ) -> Path:
+        if isinstance(cmp, Component):
+            return (
+                Path(PROCESSED_DATA_DIR)
+                .joinpath(self.file_model_type)
+                .joinpath(str(config.id))
+                .joinpath(str(ts.starttime_year))
+                .joinpath(cmp.network)
+                .joinpath(cmp.station)
+                .joinpath(cmp.component)
+                .joinpath(str(ts.starttime_doy))
+            )
+        elif isinstance(cmp, ComponentPair):
+            return (
+                Path(PROCESSED_DATA_DIR)
+                    .joinpath(self.file_model_type)
+                    .joinpath(str(config.id))
+                    .joinpath(str(ts.starttime_year))
+                    .joinpath(str(ts.starttime_doy))
+                    .joinpath(str(ts.starttime.month))
+                    .joinpath(cmp.component_code_pair)
+                    .joinpath(f"{cmp.component_a.network}.{cmp.component_a.station}-"
+                              f"{cmp.component_b.network}.{cmp.component_b.station}")
+            )
+        else:
+            raise TypeError(f"Expected either Component or ComponentPair. Got {type(cmp)}")
+
+    @property
+    def filepath(self):
+        return Path(self._filepath)
 
 class PPSDFile(db.Model):
     __tablename__ = "ppsd_file"
