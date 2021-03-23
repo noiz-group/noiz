f9c0b4f5b40eb77ac7fe249d95370e280b87d1cb
Damian Kula <dkula@unistra.fr>
2021-03-23 11:45:51 +0100

    Uses a custom type for casting from and to Path-like objects
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/database.py
+++ src/noiz/database.py
@@ -2,8 +2,12 @@
 """Database module, including the SQLAlchemy database object and DB-related utilities."""
 
 # Alias common SQLAlchemy names
+from pathlib import Path
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.types as types
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,3 +15,18 @@ migrate = Migrate()
 
 Column = db.Column
 relationship = db.relationship
+
+
+class PathInDB(types.TypeDecorator):
+    '''
+    Casts a :py:class:`pathlib.Path` object to string when adding to DB, brings it back to
+     :py:class:`pathlib.Path` on the way back
+    '''
+
+    impl = types.UnicodeText
+
+    def process_bind_param(self, value, dialect):
+        return str(value)
+
+    def process_result_value(self, value, dialect):
+        return Path(value)
