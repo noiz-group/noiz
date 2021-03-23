71b14fb95e22e23bb4fe67ff44ffcb11ca0593ff
Damian Kula <dkula@unistra.fr>
2021-03-23 12:51:46 +0100

    Moves custom db type to models
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/database.py
+++ src/noiz/database.py
@@ -2,12 +2,9 @@
 """Database module, including the SQLAlchemy database object and DB-related utilities."""
 
 # Alias common SQLAlchemy names
-from pathlib import Path
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy.types as types
-
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -15,18 +12,3 @@ migrate = Migrate()
 
 Column = db.Column
 relationship = db.relationship
-
-
-class PathInDB(types.TypeDecorator):
-    '''
-    Casts a :py:class:`pathlib.Path` object to string when adding to DB, brings it back to
-     :py:class:`pathlib.Path` on the way back
-    '''
-
-    impl = types.UnicodeText
-
-    def process_bind_param(self, value, dialect):
-        return str(value)
-
-    def process_result_value(self, value, dialect):
-        return Path(value)
