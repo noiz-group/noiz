8f9bfaa76790ad33f62a4b4e22847e056a322129
Damian Kula <dkula@unistra.fr>
2021-03-29 20:24:17 +0200

    Move NotNullColumn to noiz.database to avoid circular imports
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- src/noiz/database.py
+++ src/noiz/database.py
@@ -2,6 +2,9 @@
 """Database module, including the SQLAlchemy database object and DB-related utilities."""
 
 # Alias common SQLAlchemy names
+from functools import partial
+
+from typing import Type
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -12,3 +15,5 @@ migrate = Migrate()
 
 Column = db.Column
 relationship = db.relationship
+NullColumn: Type[db.Column] = partial(db.Column, nullable=True)  # type: ignore
+NotNullColumn: Type[db.Column] = partial(db.Column, nullable=False)  # type: ignore
