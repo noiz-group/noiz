742dd35d2e00d560c7e8e9e820dd05569cc6f7df
Damian Kula <dkula@unistra.fr>
2021-04-06 09:37:36 +0200

    Bumps version in setup and __version__
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     include_package_data=True,
     author="Damian Kula",
     author_email="dkula@unistra.fr",
-    version="0.5.2020290",
+    version="0.5.20210405",
     entry_points={"console_scripts": ["noiz=noiz.cli:cli"]},
     install_requires=[
         "flask",
