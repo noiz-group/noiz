10dc96f2ccde5f9a80248522154051a9f94dde98
Damian Kula <heavelock@gmail.com>
2023-09-14 19:54:05 +0000

    Adds a __main__ entrypoint with call to cli so noiz can be called via python -m

---
+++ src/noiz/__main__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: CECILL-B
+# Copyright © 2015-2019 EOST UNISTRA, Storengy SAS, Damian Kula
+# Copyright © 2019-2023 Contributors to the Noiz project.
+
+from noiz.cli import cli
+
+if __name__ == "__main__":
+    cli()
